af5586a68f7af364ad17cbbb9d3c23f531d3cb04
ubuntu <ubuntu@kria.mynetworksettings.com>
2024-07-31 18:42:26 -0400

    Serial coms

--- Part_3_Software/High_Level/chef.py
+++ Part_3_Software/High_Level/chef.py
@@ -3,19 +3,33 @@
 # Import our Serial libraries so we can communicate with the Teensy
 import serial
 import serial.tools.list_ports
+import numpy as np
+import time
 
 # Find the Teensy's USB port
 ports = list(serial.tools.list_ports.comports())
 for p in ports:
     if "USB Serial" in p.description:
+        print(p.device)
+        print("Connected!")
         break
 
 # Connect to that port
 arm = serial.Serial(port = p.device, baudrate = 115200, timeout = .1)
 
-arm.write(b"p")
-resp = arm.readline().decode('utf-8').rstrip("; ")
-print(resp)
+# Poll the current arm angles
+arm.write(b"p\n")
+
+# Wait for Teensy's response
+while arm.in_waiting == 0:
+	continue
+
+# Grab the data
+resp = arm.readline().decode('utf-8').rstrip(";\r\n").split(";")
+
+# Convert strings to floats
+angles = np.array(resp).astype(float).tolist()
+print(angles)
 
 
 # command = f';;;;;\n'
